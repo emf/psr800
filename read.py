df143e98f4e9f1f2770fd8f98bdbb2e3c007a60d
Erik Fichtner <emf@obfuscation.org>
2011-08-01 10:02:40 -0400

    Tidy up the recordings directories! make a sqlite database!
    Okay.  Move everything out of the horrid psr-800 rec/ tree, make a database entry
    for each file, so that we can search this crap later.

--- read.py
+++ read.py
@@ -60,7 +60,7 @@ class Au_read:
     	from datetime import datetime
         d = {}
         d['datetime'] = datetime( ord(self._info[11]) + 1900, ord(self._info[9]) + 1,  ord(self._info[7]),
-        									ord(self._info[5]), ord(self._info[3]), ord(self._info[1]))
+                                    ord(self._info[5]), ord(self._info[3]), ord(self._info[1]))
         #d['type'] -- dunno where this is stored, may be a database lookup.
         #d['length'] -- may be computed.
         d['alpha tag'] = self._info[19:self._info.find('\x00',19)]
@@ -143,17 +143,54 @@ class Au_read:
 
 sunau.Au_read = Au_read
 
+def uuidT(epochseconds):
+    from uuid import UUID, getnode, RFC_4122
+    """Generate a UUID based on given time and hardware address."""
+    from random import randrange
+    nanoseconds = int(epochseconds * 1e9)
+    # 0x01b21dd213814000 is the number of 100-ns intervals between the
+    # UUID epoch 1582-10-15 00:00:00 and the Unix epoch 1970-01-01 00:00:00.
+    timestamp = int(nanoseconds/100) + 0x01b21dd213814000
+    clock = randrange(1<<16) # don't use stable storage
+    time_low = timestamp & (0x100000000 - 1)
+    time_mid = (timestamp >> 32) & 0xffff
+    time_hi_ver = (timestamp >> 48) & 0x0fff
+    clock_low = clock & 0xff
+    clock_hi_res = (clock >> 8) & 0x3f
+    node = getnode()
+    uuid = UUID(fields=(time_low, time_mid, time_hi_ver, clock_low, clock_hi_res, node) )
+    return uuid
+
 import os
 import fnmatch
+from time import mktime
+from datetime import datetime
+import shutil
+import sqlite3
 
 inputdir = "."
-outputdir = "../tmp"
+outputdir = "../tmp/"
+
+conn = sqlite3.connect(outputdir+"recordings.db")
+c = conn.cursor()
+try:
+    c.execute('create table rec (freq text, name text, time text, filename text);')
+    c.execute('create index f_idx on rec(freq);')
+    c.execute('create index n_idx on rec(name);')
+except sqlite3.OperationalError:
+    pass
 
 for root, dirs, files in os.walk(inputdir):
-	for filename in fnmatch.filter(files, "*.AU"):
-		objname = os.path.join(root, filename)
-		print objname
-		f = sunau.open(objname, 'r')
-		print f.getmetadata()
-		f.close()
+    for filename in fnmatch.filter(files, "*.AU"):
+        objname = os.path.join(root, filename)
+        f = sunau.open(objname, 'r')
+        M =  f.getmetadata()
+        f.close()
+        # generate a new unique filename; a uuid based on the recording's time
+        t = int(mktime(M['datetime'].timetuple()))      # time in epochseconds -> rec.time
+        newf = str(uuidT(t)) + ".au"                    # new file name -> rec.filename
+        print objname, "->", outputdir+newf
+        c.execute('insert into rec values(?,?,?,?);', (M['freq'], M['alpha tag'], t, newf) )
+        shutil.move(objname, outputdir+newf)
+    conn.commit()
 
